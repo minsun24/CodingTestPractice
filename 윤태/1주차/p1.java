ab3e767f0f65dd51800e0c011ec9b3be9b4e2981
cxzaqq <roy1228@naver.com>
2025-01-12 22:52:11 +0900

    update

--- 윤태/1주차/p1.java
+++ 윤태/1주차/p1.java
@@ -1,13 +1,13 @@
-import java.util.Arrays;
-
 public class p1 {
     public static void main(String[] args) {
         String str1 = "The Curious Case of Benjamin Button";
         String str2 = " The first character is a blank";
         String str3 = "The last character is a blank ";
         String str4 = "Continuos    blanks";
+        String str5 = "";
+        String str6 = " ";
 
-        String[] strs = {str1, str2, str3, str4};
+        String[] strs = {str1, str2, str3, str4, str5, str6};
 
         for(String str : strs) {
             int result = solution(str);
@@ -16,9 +16,11 @@ public class p1 {
     }
 
     public static int solution(String myString) {
-        String[] strArr = myString.trim().split("\\s+");
-        System.out.println(Arrays.toString(strArr));
-        System.out.println(strArr.length);
-        return strArr.length;
+        if(myString.trim().isEmpty()) {
+            return 0;
+        } else {
+            String[] words = myString.trim().split("\\s+");
+            return words.length;
+        }
     }
 }
