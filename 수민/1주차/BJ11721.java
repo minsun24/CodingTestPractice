4de98ec491ffcf07f25f75cbd438cf75445259df
Sumin <71063214+emily9949@users.noreply.github.com>
2025-01-13 18:46:35 +0900

    [1주차] 11721 - 열 개씩 끊어 출력하기

--- 수민/1주차/BJ11721.java
+++ 수민/1주차/BJ11721.java
@@ -7,7 +7,7 @@ public class BJ11721 {
 
         String s = sc.next();
 
-        // index 를 0-9 씩 끊어서 출력
+        // index 를 0-9 씩 끊어서 출력.
         for(int i = 0; i < s.length(); i++) {
             System.out.print(s.charAt(i));
             if(i % 10 == 9) {
