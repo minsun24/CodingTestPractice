9700332c20ab453bda1f41e65c75e7edf8a570c3
Sumin <71063214+emily9949@users.noreply.github.com>
2025-01-13 18:47:24 +0900

    [1주차] 27866 - 문자와 문자열

--- 수민/1주차/BJ27866.java
+++ 수민/1주차/BJ27866.java
@@ -8,7 +8,7 @@ public class BJ27866 {
         String s = sc.next();
         int n = sc.nextInt();
 
-        // charAt 함수 : String으로 저장된 문자열 중에서 한 글자만 선택해서 변환해줌
+        // charAt 함수 : String으로 저장된 문자열 중에서 한 글자만 선택해서 변환해줌.
         System.out.println(s.charAt(n-1));
 
         sc.close();
