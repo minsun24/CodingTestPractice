239dbdc14e5a2a3de84107246a5b887fc2298d14
minsun24 <jms49526@gmail.com>
2025-01-13 20:15:51 +0900

    week1 : boj1919(애너ê그램 ㅏ만들기)

--- 민선/week1/BJ1259.java
+++ 민선/week1/BJ1259.java
@@ -1,4 +1,4 @@
-/* 11259번 : 팰린드롬인지 확인하기
+/* 1259번 : 팰린드롬인지 확인하기
     # 접근 : 문자열로 입력 받아서 팰린드롬인지 확인하기
         팰린드롬 확인 메서드 - 문자열 양쪽(앞/뒤)로 확인
     #
@@ -10,7 +10,12 @@ import java.util.*;
 
 import java.util.Scanner;
 
-public class BJ1259 {
+import java.util.*;
+
+
+import java.util.Scanner;
+
+public class BJ1259{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -19,6 +24,9 @@ public class BJ1259 {
             if (str.equals("0")) {
                 break;  // 0을 입력받으면 종료
             }
+
+            // 입력값 출력
+
             // 팰린드롬 여부 판단
             if (isPalindrome(str)) {
                 System.out.println("yes");
---
+++ 민선/week1/BJ1919.java
@@ -0,0 +1,42 @@
+/*  1919 번 : 애너그램 만들기
+    알파벳 개수 같으면 애너그램 가능
+
+
+ */
+import java.util.*;
+
+public class BJ1919 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String first = sc.nextLine();
+        String second = sc.nextLine();
+
+        int[] a = new int['z' - 'a' + 1];
+        int[] b = new int['z' - 'a' + 1];
+
+
+        for(int i=0; i<first.length(); i++){
+            char a_c = first.charAt(i);
+            a[a_c - 'a'] ++;
+        }
+
+        for(int i=0; i<second.length(); i++){
+            char b_c = second.charAt(i);
+            b[b_c - 'a'] ++;
+        }
+        int cnt = 0;
+
+        for(int i=0; i<a.length; i++){
+            if(a[i] != b[i]){
+                int sum = (a[i] - b[i]);
+
+                if(sum < 0){
+                    sum *= -1;
+                }
+                cnt += sum;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
