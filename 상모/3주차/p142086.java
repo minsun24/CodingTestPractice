fd35ce3e2bff6aab8c9022ec5c14bf568a003a0a
morris <dltkdah777@gmail.com>
2025-01-31 20:31:26 +0900

    프로그래머스 가장 가까운 같은 글자 다시풀기 (주석 추가)

--- 상모/3주차/p142086.java
+++ 상모/3주차/p142086.java
@@ -9,17 +9,22 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             
-            // System.out.println("c : "+c);
+            // 2. 밑에 put한 map을 통해서 그 다음 값 유무 확인
+            System.out.println("char : "+c);
             if (lastIdx.containsKey(c)) {
+                // 3. 현재 i (index)에서 기존에 put한 lastIdx를 빼면 위치를 얻을수 있음
                 answer[i] = i - lastIdx.get(c);
-                // System.out.println("true : "+lastIdx.get(c));
+                System.out.println("true : "+lastIdx.get(c));
+                System.out.println("i - lastIdx : " + (i-lastIdx.get(c)));
             } else {
                 answer[i] = -1;
-                // System.out.println("false : "+lastIdx.get(c));
+                System.out.println("false : "+lastIdx.get(c));
             }
             
+            // 1. 어찌됐든 put
             lastIdx.put(c, i);
-            // System.out.println("put : "+lastIdx.get(c));
+            System.out.println("put : "+lastIdx.get(c));
+            System.out.println(" ");
         }
         
         return answer;
