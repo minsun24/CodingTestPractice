d7ae66dd94be9d6e27b714c41e522f57bd2dbf8c
morris <dltkdah777@gmail.com>
2025-02-04 22:10:14 +0900

    프로그래머스 인덱스 바꾸기 다시풀기 String.valueOf()사용

--- 상모/3주차/p120895.java
+++ 상모/3주차/p120895.java
@@ -1,16 +1,13 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        StringBuilder answer = new StringBuilder();
+        char[] ch = my_string.toCharArray();
         
-        for (int i = 0; i < my_string.length(); i++) {
-            if (i == num1) {
-                answer.append(my_string.charAt(num2));    
-            } else if (i == num2) {
-                answer.append(my_string.charAt(num1));
-            } else answer.append(my_string.charAt(i));
-            
-        }
+        ch[num1] = my_string.charAt(num2);
+        ch[num2] = my_string.charAt(num1);
         
-        return answer.toString();
+        // String,valueOf는 Object값을 String으로 변환할때 쓰며
+        // NPE를 회피할수 있다고 한다. 따라서 toString() 보다 이것의 사용을 추천하고 있다.
+        String answer = String.valueOf(ch);
+        return answer;
     }
 }
