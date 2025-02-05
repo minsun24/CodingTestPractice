52382a56ef0b31fd64d5acd869dfe6b52a69fcb2
Jooahyeon <dkgus0496@naver.com>
2025-02-05 15:44:46 +0900

    BJ2752

---
+++ 아현/3주차/Algorithm03/src/array/Threenumber.java
@@ -0,0 +1,37 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Threenumber {
+    public static void main(String[] args) {
+        //BJ2752
+
+        /*
+문제
+동규는 세수를 하다가 정렬이 하고 싶어졌다.
+
+정수 세 개를 생각한 뒤에, 이를 오름차순으로 정렬하고 싶어졌다.
+
+정수 세 개가 주어졌을 때, 가장 작은 수, 그 다음 수, 가장 큰 수를 출력하는 프로그램을 작성하시오.
+
+입력
+정수 세 개가 주어진다. 이 수는 1보다 크거나 같고, 1,000,000보다 작거나 같다. 이 수는 모두 다르다.
+
+출력
+제일 작은 수, 그 다음 수, 제일 큰 수를 차례대로 출력한다.
+*/
+        //세 수 입력
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[3];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+            Arrays.sort(numbers);
+        }
+
+        //세 수 출력
+        for (int i : numbers) {
+            System.out.println(i + " ");
+        }
+    }
+}
