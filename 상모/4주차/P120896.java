a5a8635501109ebf42cb159324aef9079f2ac1b3
morris <dltkdah777@gmail.com>
2025-02-11 13:24:24 +0900

    프로그래머스 한 번만 등장한 문자 (완전탐색)

--- 상모/4주차/P120896.java
+++ 상모/4주차/P120896.java
@@ -1,23 +1,45 @@
 import java.util.*;
 
+// class P120896 {
+//     public String solution(String s) {
+//         Map<Character, Integer> countMap = new HashMap<>();
+        
+//         for (char ch : s.toCharArray()) {
+//             countMap.put(ch, countMap.getOrDefault(ch, 0) + 1);
+//         }
+        
+//         StringBuilder answer = new StringBuilder();
+//         for (char ch : countMap.keySet()) {
+//             if (countMap.get(ch) == 1) {
+//                 answer.append(ch);
+//             }
+//         }
+        
+//         char[] result = answer.toString().toCharArray();
+//         Arrays.sort(result);
+        
+//         return new String(result);
+//     }
+// }
+
 class P120896 {
     public String solution(String s) {
-        Map<Character, Integer> countMap = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
         
-        for (char ch : s.toCharArray()) {
-            countMap.put(ch, countMap.getOrDefault(ch, 0) + 1);
-        }
-        
-        StringBuilder answer = new StringBuilder();
-        for (char ch : countMap.keySet()) {
-            if (countMap.get(ch) == 1) {
-                answer.append(ch);
+        for (int i = 0; i < s.length(); i++) {
+            int count = 0;
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    count++;
+                }
+            }
+            if (count <= 1) {
+                sb.append(s.charAt(i));
             }
         }
         
-        char[] result = answer.toString().toCharArray();
-        Arrays.sort(result);
-        
-        return new String(result);
+        char[] chArr = sb.toString().toCharArray();
+        Arrays.sort(chArr);
+        return new String(chArr);
     }
 }
