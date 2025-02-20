5afeb03516e0164730654ea56d177f2cfb5480df
Morris235 <dltkdah777@gmail.com>
2025-02-20 20:24:36 +0900

    팩토리얼(재귀) 분석

--- morris/week5/BJ10870.java
+++ morris/week5/BJ10870.java
@@ -15,11 +15,12 @@ public class BJ10870 {
     }
 
     // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
-    public static void fibonacci(int n, int v) {
+    // 1+1=2, 1+2=3, 2+3=5, 3+5=8,
+    public static int fibonacci(int n, int v) {
         // System.out.println(v);
         n++;
-        System.out.println(n);
         // 10까지 반복됨
-        if (n < 10) fibonacci(n++, v + v);
+        if (n < 10) return 1;
+        return fibonacci(n, v+v);
     }
 }
