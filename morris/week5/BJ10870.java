b28af8fb83cb0a594698b072b5b822bae9aea9d8
Morris235 <dltkdah777@gmail.com>
2025-02-20 19:35:45 +0900

    피보나치 수 5 (재귀호출 연습)

--- morris/week5/BJ10870.java
+++ morris/week5/BJ10870.java
@@ -1,21 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class BJ10870 {
     public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //    int n = Integer.parseInt(br.readLine());
+    Scanner sc = new Scanner(System.in);
 
-       int n = Integer.parseInt(br.readLine());
+    int n = sc.nextInt();
 
-       favonachi(n, 1);
+    fibonacci(n, 1);
     }
 
     // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
-    public static int favonachi(int n, int v) {
-        int count = 1;
-        if (count <= n) favonachi(count+1, v + v);
-
-        return n;
+    public static void fibonacci(int n, int v) {
+        // System.out.println(v);
+        n++;
+        System.out.println(n);
+        // 10까지 반복됨
+        if (n < 10) fibonacci(n++, v + v);
     }
 }
