539281c4377cece952a131c38a4603120f8eb10c
Morris235 <dltkdah777@gmail.com>
2025-02-22 22:16:54 +0900

    피보나치 수 5 (재귀연습)

--- morris/week5/BJ10870.java
+++ morris/week5/BJ10870.java
@@ -7,20 +7,20 @@ public class BJ10870 {
     public static void main(String[] args) throws IOException {
     //    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     //    int n = Integer.parseInt(br.readLine());
-    Scanner sc = new Scanner(System.in);
+    // Scanner sc = new Scanner(System.in);
 
-    int n = sc.nextInt();
+    // int n = sc.nextInt();
 
-    fibonacci(n);
+    int result = fibonacci(10);
+    System.out.println(result);
     }
 
     // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
     // 1+1=2, 1+2=3, 2+3=5, 3+5=8,
+    // 재귀함수를 이용하여 n번째 피보나치 수를 계산하는 함수 (F(0)=0, F(1)=1)
     public static int fibonacci(int n) {
-        // System.out.println(v);
-        n++;
-        // 10까지 반복됨
-        if (n < 10) return 1;
-        return fibonacci(n);
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
